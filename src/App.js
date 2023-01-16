650cb54e84fdfad79551868bf66afd4de7a2ae84
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-16 11:24:17 +0330

    Add components to App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./feature/store";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
-        <div className='App Container'></div>
+        <div className='App Container'>
+          <AddTodoForm />
+          <TodoList />
+        </div>
       </div>
     </Provider>
   );
