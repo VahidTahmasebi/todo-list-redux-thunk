c57b2c80ae3cbc5140f891fe4ce3636fdf593e0d
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-16 08:39:27 +0330

    Add form operations

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const AddTodoForm = () => {
+  const [valueInput, setValueInput] = useState("");
+
+  // form handler
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
       <label className='sr-only'>Name</label>
@@ -6,6 +15,8 @@ const AddTodoForm = () => {
         type='text'
         className='form-control mb2 mr-sm-2'
         placeholder='Add todo...'
+        value={valueInput}
+        onChange={(e) => setValueInput(e.target.value)}
       />
       <button type='submit' className='btn btn-primary mb-2'>
         Add todo
