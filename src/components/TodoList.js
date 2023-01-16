c1c10686022536c3531d13c7674b3eb6de0ecb8c
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-16 11:27:08 +0330

    Add initial mount via use effect

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,16 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAsyncTodos } from "../feature/todosSlice";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   const { todos, loading, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  // initial mount
+  useEffect(() => {
+    dispatch(getAsyncTodos());
+  });
 
   if (loading) return <p>loading...</p>;
   if (error) return <p>{error}</p>;
