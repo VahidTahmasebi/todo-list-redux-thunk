513afe4a88eca7aeae95a2a3801693526151dc9a
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-01-16 12:21:09 +0330

    Add totalCompleteItem syntax

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { store } from "./feature/store";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
+import TotalCompleteItem from "./components/TotalCompleteItems";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='App'>
         <div className='App Container'>
           <AddTodoForm />
+          <TotalCompleteItem />
           <TodoList />
         </div>
       </div>
